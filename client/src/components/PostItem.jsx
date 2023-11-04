3a0bae144524ba9e5c62d771b9ec35d37cf9c42a
fedotick <feodortcaciuc2004@gmail.com>
2023-11-04 20:09:41 +0200

    worked with posts

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -13,27 +13,34 @@ export const PostItem = ({ post }) => {
     }
 
     return (
-        <Link to={`/${post._id}`}>
-            <div className='flex flex-col basis-1/4 flex-grow'>
-                <div className='flex justify-between items-center pt-2'>
-                    <div className='text-xs text-white opacity-50'>
-                        {post.username}
-                    </div>
-                    <div className='text-xs text-white opacity-50'>
-                        <Moment date={post.createdAt} format='D MMM YYYY' />
-                    </div>
+        <Link to={`/${post._id}`} className='hover:bg-[#171717] transition-all duration-300'>
+            <div className='flex flex-col mx-auto border rounded-md border-gray overflow-hidden'>
+                <div
+                    className={post.imageURL ? 'flex rounded-sm h-80' : 'flex rounded-sm'}
+                >
+                    {post.imageURL && (
+                        <img src={`http://localhost:7777/${post.imageURL}`} alt="IMG" className='object-cover w-full' />
+                    )}
                 </div>
-                <div className='text-white text-xl'>{post.title}</div>
-                <p className='text-white opacity-60 text-xs pt-4 line-clamp-4'>
-                    {post.text}
-                </p>
-                <div className='flex gap-3 items-center mt-2'>
-                    <button className='flex items-center justify-center gap-2 text-xs text-white opacity-50'>
-                        <AiFillEye /> <span>{post.views}</span>
-                    </button>
-                    <button className='flex items-center justify-center gap-2 text-xs text-white opacity-50'>
-                        <AiOutlineMessage /> <span>{post.comments?.length || 0}</span>
-                    </button>
+                <div className='flex flex-col gap-y-[15px] px-[30px] py-[25px]'>
+                    <div>
+                        <div className='text-lg font-normal'>
+                            {post.username}
+                        </div>
+                        <div className='text-xs text-zinc-400'>
+                            <Moment date={post.createdAt} format='D MMM YYYY' />
+                        </div>
+                    </div>
+                    <div className='font-black text-3xl'>{post.title}</div>
+                    <p className='text-sm text-gray line-clamp-4'>{post.text}</p>
+                    <div className='flex gap-3 items-center justify-end mt-2'>
+                        <button className='flex items-center justify-center gap-2 text-xs text-white opacity-50'>
+                            <AiFillEye /> <span>{post.views}</span>
+                        </button>
+                        <button className='flex items-center justify-center gap-2 text-xs text-white opacity-50'>
+                            <AiOutlineMessage /> <span>{post.comments?.length || 0}</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </Link>
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -20,7 +20,7 @@ export const MainPage = () => {
     }
 
     return (
-        <div className='max-w-[900px] mx-auto py-10'>
+        <div className='flex flex-col w-1/2 mx-auto py-10'>
             <div className='flex justify-between gap-8'>
                 <div className='flex flex-col gap-10 basis-4/5'>
                     {posts?.map((post, index) => (
