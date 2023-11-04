bd7d0b6e8abbb794376070cdc544fe5d0833981c
fedotick <feodortcaciuc2004@gmail.com>
2023-11-04 19:29:34 +0200

    started posts and main page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllPosts } from '../redux/features/post/postSlice'
+import { PostItem } from '../components/PostItem'
 
 export const MainPage = () => {
+    const { posts } = useSelector(state => state.post)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getAllPosts())
+    }, [dispatch])
+
+    if (!posts.length) {
+        return (
+            <div className='text-xl text-center py-10'>
+                Posts haven't been created yet
+            </div>
+        )
+    }
+
     return (
-        <div>MainPage</div>
+        <div className='max-w-[900px] mx-auto py-10'>
+            <div className='flex justify-between gap-8'>
+                <div className='flex flex-col gap-10 basis-4/5'>
+                    {posts?.map((post, index) => (
+                        <PostItem post={post} key={index} />
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
