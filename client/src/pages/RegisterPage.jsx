404d5f6071c10402c54d2f76e49cc84633912547
fedotick <feodortcaciuc2004@gmail.com>
2023-11-04 19:33:06 +0200

    RegisterPage adaptive

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -34,47 +34,47 @@ export const RegisterPage = () => {
             onSubmit={e => e.preventDefault()}
             className='w-80 lg:w-1/3 flex flex-col mx-auto mt-10 p-10 border rounded-lg border-gray'
         >
-            <h2 className='font-black text-2xl text-center'>Join Peacemaking</h2>
-            <p className='font-light text-sm my-5 tracking-[0.90px]'>Get more opportunities and privileges by joining the most useful community for residents from both banks of the Nistru River</p>
-            <label className='font-medium text-lg flex flex-col mb-[15px]'>
+            <h2 className='font-black text-xl lg:text-2xl text-center'>Join Peacemaking</h2>
+            <p className='font-light text-xs lg:text-sm my-5 tracking-[0.90px]'>Get more opportunities and privileges by joining the most useful community for residents from both banks of the Nistru River</p>
+            <label className='font-medium text-base lg:text-lg flex flex-col mb-[15px]'>
                 Input your username: 
                 <input 
                     type="text" 
                     value={username}
                     onChange={e => setUsername(e.target.value)}
                     placeholder='Username'
-                    className='p-3 text-sm text-black outline-none rounded-md'
+                    className='p-3 text-xs lg:text-sm text-black outline-none rounded-md bg-[#444]'
                 />
             </label>
-            <label className='font-medium text-lg flex flex-col mb-[15px]'>
+            <label className='font-medium text-base lg:text-lg flex flex-col mb-[15px]'>
                 Input your email:
                 <input 
                     type="email" 
                     value={email}
                     onChange={e => setEmail(e.target.value)}
                     placeholder='Email'
-                    className='p-3 text-sm text-black outline-none rounded-md'
+                    className='p-3 text-xs lg:text-sm text-black outline-none rounded-md bg-[#444]'
                 />
             </label>
-            <label className='font-medium text-lg flex flex-col'>
+            <label className='font-medium text-base lg:text-lg flex flex-col'>
                 Type your password: 
                 <input 
                     type="password" 
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                     placeholder='Password'
-                    className='p-3 text-sm text-black outline-none rounded-md'
+                    className='p-3 text-xs lg:text-sm text-black outline-none rounded-md bg-[#444]'
                 />
             </label>
             <div className='mt-[27px]'>
                 <button
                     type="submit"
                     onClick={handleSubmit} 
-                    className='bg-orange w-full font-black text-sm py-3 uppercase rounded-md hover:bg-orangeHover transition-all duration-300'
+                    className='bg-orange w-full font-black text-xs lg:text-sm py-3 uppercase rounded-md hover:bg-orangeHover transition-all duration-300'
                 >
                     Register
                 </button>
-                <div className='flex gap-x-2 mt-3'>
+                <div className='text-sm lg:text flex gap-x-1 lg:gap-x-2 mt-3'>
                     <p>Already have an account?</p>
                     <Link to={'/login'} className='text-blue font-medium'>Login now</Link>
                 </div>
